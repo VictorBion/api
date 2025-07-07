fcdd4868c63b860370b92741f2e0c78fbbafa1bf
vitor <vitorbion@gmail.com>
2025-07-07 04:35:44 -0300

    feat: add repositories

--- src/main/java/com/eventostec/api/repositories/CouponRepository.java
+++ src/main/java/com/eventostec/api/repositories/CouponRepository.java
@@ -1,8 +1,11 @@
 package com.eventostec.api.repositories;
 
+
+
+import com.eventostec.api.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CouponRepository  extends JpaRepository<JpaRepository, UUID> {
+public interface CouponRepository  extends JpaRepository<Coupon, UUID> {
 }
